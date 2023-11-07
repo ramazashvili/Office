90e9850b0728553b1102d6a7fa2b40c76c75b6a1
ramazashvili <126188768+ramazashvili@users.noreply.github.com>
2023-11-07 19:35:27 +0400

    first commit

---
+++ src/app/page/apiservices.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiservicesService {
+
+  constructor(private http:HttpClient) { }
+
+  apiurl = "https://api.npoint.io/d8974e2a83b76da4e84d";
+
+
+  homeapi():Observable<any> {
+    return this.http.get(`${this.apiurl}`);
+  }
+
+}
